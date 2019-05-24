617c0673fae308b160d9e3428b3d855063622b7d
Tom White <eugene.white@gmail.com>
2019-05-24 17:36:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -35,7 +35,6 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect {do} |element, index|
-    index != 1 ? element << "s" : element
-  end
+  array.each_with_index.collect { |element, index| index != 1 ? element << "s" : element
+  }
 end
